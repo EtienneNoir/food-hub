3387b94ebc317c95ee76f622c08282bc5741609d
EtienneNoir <etiennebanza37@gmail.com>
2024-12-24 01:19:49 +0200

    added a responsive layout

--- recipes/src/App.tsx
+++ recipes/src/App.tsx
@@ -1,7 +1,29 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 const App = () => {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    // creating break points, to specify how the website should look on different devices
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`, // mobile devices
+        lg: `"nav nav" "aside main"`, // devices larger than 1024px
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        {/** Will only show on lager devices */}
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 };
 
 export default App;
